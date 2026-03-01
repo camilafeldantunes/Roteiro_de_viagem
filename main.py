187e0b39c618817bcc3c6f81d2e5ffc287247e3d
Camila Feldkircher Antunes <camilafeldkircherantunes@MacBook-Air-de-Camila.local>
2026-03-01 15:54:14 -0300

    feat: utilizando LangChain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from openai import OpenAI
+from langchain.schema import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
@@ -7,25 +8,17 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 numero_dias = 7
 numero_criancas = 2
-atividade = "música"
+atividade = "praia"
 
-prompt = f"Crie um roteiro de viagem de {numero_dias} dias, para uma família com {numero_criancas} crianças, que gosta de {atividade}"
-
-
-cliente = OpenAI(api_key=api_key)
-
-resposta = cliente.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        { 
-            "role" : "system",
-            "content" : "Você é um assistente de roteiro de viagens"
-        },
-        { 
-            "role" : "user",
-            "content" : prompt
-        }
-    ]
+mensagens = [
+    SystemMessage(content="Você é um estagiario feliz e esta com muita vontade de responder as pessoas. voce trabalha em uma agencia de viagens. responda sempre da melhor maneira possivel e com muitos emojis"),
+    HumanMessage(content=f"Crie um roteiro de viagens para {numero_dias} dias para uma família com {numero_criancas} crianças que busca atividades relacionadas a {atividade}.")
+]
+modelo = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.5,
+    api_key=api_key
 )
 
-print(resposta.choices[0].message.content)
+resposta = modelo.invoke(mensagens)
+print(resposta.content)
