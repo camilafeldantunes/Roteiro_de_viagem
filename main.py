d4bba7a711fdfbe8dfce2fe246266408e4dfa8da
Camila Feldkircher Antunes <camilafeldkircherantunes@MacBook-Air-de-Camila.local>
2026-03-16 11:02:15 -0300

    feat: resposta em formato json

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
 from langchain.schema import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
 
+set_debug(True)
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-numero_dias = 7
-numero_criancas = 2
-atividade = "praia"
 
-modelo_de_prompt = PromptTemplate(
+class Destino(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar")
+    motivo: str = Field("O motivo pelo qual é interessante visitar essa cidade")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
+prompt_cidade = PromptTemplate(
     template=""""
-        Crie um roteiro de viagem de {dias} dias,
-        para uma família com {numero_criancas} crianças,
-        que gostam de {atividade}
-    """
-)
+       Sugira uma cidade dado o meu interesse por {interesse}.
+       {formato_de_saida}
 
-prompt = modelo_de_prompt.format(
-    dias=numero_dias,
-    numero_criancas=numero_criancas,
-    atividade=atividade
+    """,
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
-
-print("Prompt: \n", prompt)
 
 modelo=ChatOpenAI(
     model="gpt-3.5-turbo",
     temperature=0.5,
     api_key=api_key
 )
+cadeia = prompt_cidade | modelo | parseador
 
-resposta = modelo.invoke(prompt)
-print(resposta.content)
+resposta = cadeia.invoke(
+    {
+        "interesse": "neve"
+    }
+)
+print(resposta)
