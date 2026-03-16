7c533ccf9bef19270b1bbbf5fdaadea94634556d
Camila Feldkircher Antunes <camilafeldkircherantunes@MacBook-Air-de-Camila.local>
2026-03-16 18:34:50 -0300

    feat: assistente utilizando LangGraph

--- main_langgraph.py
+++ main_langgraph.py
@@ -3,6 +3,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from typing import Literal, TypedDict
 from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
+import asyncio
 import os
 
 load_dotenv()
@@ -44,14 +47,45 @@ prompt_roteador = ChatPromptTemplate.from_messages(
 
 roteador = prompt_roteador | modelo.with_structured_output(Rota)
 
-def responde(pergunta : str):
-    rota = roteador.invoke({
-        "query": pergunta
-    })["destino"]
-    print(rota)
-    
-    if rota == "praia":
-        return cadeia_praia.invoke({"query": pergunta})
-    return cadeia_montanha.invoke({"query": pergunta})
+class Estado(TypedDict):
+    query: str
+    destino: Rota
+    resposta: str
 
-print(responde("Quero passear por praias belas no Brasil"))
+async def no_roteador(estado: Estado, config=RunnableConfig):
+    return {
+        "destino": await roteador.ainvoke({"query":estado["query"]}, config)
+    }
+
+async def no_praia(estado: Estado, config=RunnableConfig):
+    return {
+        "resposta": await cadeia_praia.ainvoke({"query":estado["query"]}, config)
+    }
+
+async def no_montanha(estado: Estado, config=RunnableConfig):
+    return {
+        "resposta": await cadeia_montanha.ainvoke({"query":estado["query"]}, config)
+    }
+
+def escolhe_no(estado: Estado)->Literal["praia", "montanha"]:
+    return "praia" if estado["destino"]["destino"] == "praia" else "montanha"
+
+grafo = StateGraph(Estado)
+grafo.add_node("rotear", no_roteador)
+grafo.add_node("praia", no_praia)
+grafo.add_node("montanha", no_montanha)
+
+grafo.add_edge(START, "rotear")
+grafo.add_conditional_edges("rotear", escolhe_no)
+grafo.add_edge("praia", END)
+grafo.add_edge("montanha", END)
+
+app = grafo.compile()
+
+async def main():
+    resposta = await app.ainvoke({
+        "query": "quero escalar uma montanha no chile"
+    })
+    print(resposta["resposta"])
+
+asyncio.run(main())
